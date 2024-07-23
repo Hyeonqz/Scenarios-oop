ff856e50c01aefad14ac58d0854ec791863f8f26
jinhyeongyu <wlsgusrb78@naver.com>
2024-07-23 17:13:10 +0900

    init

---
+++ 티켓 판매 어플리케이션/Sell-Tickets/readme.md
@@ -0,0 +1,6 @@
+# 직접 만들어보는 티켓 판매 어플리케이션
+
+> Java21, SpringBoot3.31, SpringDataJPA, MySQL, Postman
+
+
+### 구현 목표
---
+++ 티켓 판매 어플리케이션/Sell-Tickets/src/main/java/org/spring/selltickets/SellTicketsApplication.java
@@ -0,0 +1,13 @@
+package org.spring.selltickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellTicketsApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(SellTicketsApplication.class, args);
+	}
+
+}
