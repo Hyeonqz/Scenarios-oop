46300e83fab498e95134cf4a42d577e2f219a6b0
jinhyeongyu <wlsgusrb78@naver.com>
2024-07-24 10:49:47 +0900

    init other

---
+++ 티켓 판매 어플리케이션/Sell-Tickets/src/test/java/org/spring/selltickets/SellTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.selltickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellTicketsApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
