36561ca29e9375df20881e8043c9ed491cdaea0b
Hyeonqz <wlsgusrb78@naver.com>
2024-07-24 19:33:03 +0900

    init

---
+++ 티켓 판매 어플리케이션/MovieTicketing/src/test/java/kr/co/qrbank/movieticketing/MovieTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.qrbank.movieticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketingApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
